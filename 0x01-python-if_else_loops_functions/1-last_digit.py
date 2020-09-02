0a21a6fb2947ef8c2cd5f565c295c7c59d41c6fa
Andrescortes23 <andresccortes23@icloud.com>
2020-09-02 05:45:18 +0000

    adding zero case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,9 +13,11 @@ if number > 0:
         print("Last digit of", number, "is", number % 10, "and is 0")
 
 if number < 0:
-
     if number % 10 == 0:
         print("Last digit of {} is {} and is 0".format(number, number % 10))
 
     else:
         print("Last digit of", number, "is", (-((abs(number) % 10))), less)
+
+if number == 0:
+    print("Last digit of", number, "is", number % 10, "and is 0")
