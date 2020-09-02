4165e57fef1b0049dff154df249ee4a3eaa3759e
Andrescortes23 <andresccortes23@icloud.com>
2020-09-02 04:11:50 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in str:
-        if ord(a) == 32:  # if is space char
-            print("{}".format(a), end='')
-        elif ord(a) >= 48 and ord(a) < 58:  # if is a number
+        if ord(a) >= 0 and ord(a) < 97:  # if is any other char
             print("{}".format(a), end='')
         elif ord(a) >= 97 and ord(a) < 123:  # if is lowecase
             print("{:c}".format(ord(a) - 32), end='')
-        elif ord(a) > 64 and ord(a) < 91:  # if is a uppercase
-            print("{}".format(a), end='')
     print()
