b493e4bc1851e1a3c200aa4632d8ac87a72a0d0d
Andrescortes23 <andresccortes23@icloud.com>
2020-09-03 17:34:12 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-arglen = len(sys.argv) - 1  # To argv doesnt count my function as arg
-print("{} ".format(arglen), end='')
+    arglen = len(sys.argv) - 1  # To argv doesnt count my function as arg
+    print("{} ".format(arglen), end='')
 
-if arglen == 0:
-    print("{}".format("arguments."))
-elif arglen == 1:
-    print("{}".format("argument:"))
-elif arglen > 1:
-    print("{}".format("arguments:"))
+    if arglen == 0:
+        print("{}".format("arguments."))
+    elif arglen == 1:
+        print("{}".format("argument:"))
+    elif arglen > 1:
+        print("{}".format("arguments:"))
 
-iterator = 1
-while iterator != arglen + 1:
-    print("{}: {}".format(iterator, sys.argv[iterator]))
-    iterator = iterator + 1
+    iterator = 1
+    while iterator != arglen + 1:
+        print("{}: {}".format(iterator, sys.argv[iterator]))
+        iterator = iterator + 1
