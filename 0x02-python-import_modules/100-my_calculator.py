c88e91053edcd2f5bf4ad1325f657c4220ec569f
Andrescortes23 <andresccortes23@icloud.com>
2020-09-04 03:11:36 +0000

    correcting problem with mul

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,14 +4,15 @@ if __name__ == "__main__":
     import sys
 
     arglen = len(sys.argv)
-    a = int(sys.argv[1])
-    sign = sys.argv[2]
-    b = int(sys.argv[3])
 
     if arglen != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        a = int(sys.argv[1])
+        sign = sys.argv[2]
+        b = int(sys.argv[3])
+
         if sign == '+':
             print("{} {} {} = {}".format(a, sign, b, c.add(a, b)))
         elif sign == '-':
