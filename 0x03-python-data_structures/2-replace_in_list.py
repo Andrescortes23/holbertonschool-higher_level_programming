786deb7009714cafd9615494ca2089206f0100a4
Andrescortes23 <andresccortes23@icloud.com>
2020-09-08 20:17:33 +0000

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0:
+    if idx <= 0:
         return (my_list)
     elif idx > len(my_list):
         return (my_list)
-    elif idx >= 0:
+    elif idx > 0:
         for a in range(len(my_list)):
             if a == idx:
                 my_list[a - 1] = element
