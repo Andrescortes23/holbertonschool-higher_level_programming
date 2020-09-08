93a0ba05dcaf3cf667d19b3f08d4d4bdd545050b
Andrescortes23 <andresccortes23@icloud.com>
2020-09-08 19:55:45 +0000

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for a in range(len(my_list)):
-        if a == idx and idx > 0:
-            my_list[a] = element
-            return (my_list)
-        elif a == idx and idx < 0:
-            return (my_list)
-        elif idx > len(my_list):
-            return (my_list)
+
+    if idx < 0:
+        return (my_list)
+    elif idx > len(my_list):
+        return (my_list)
+    elif idx > 0:
+        for a in range(len(my_list)):
+            if a == idx:
+                my_list[a] = element
+                return (my_list)
