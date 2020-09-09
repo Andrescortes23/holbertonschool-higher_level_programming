e3b1332d61de5d3674de240a804ccab69c3f0776
Andrescortes23 <andresccortes23@icloud.com>
2020-09-09 03:02:16 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    Length = len(sentence)
-    Firstchar = sentence[0]
+    if len(sentence) == 0:
+        Firstchar = None
+        Length = len(sentence)
+    else:
+        Length = len(sentence)
+        Firstchar = sentence[0]
+
     newtuple = (Length, Firstchar)
 
     return (newtuple)
