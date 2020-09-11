91f6371fb93fb3103cdddfab14d8a78b63c3d32e
Andrescortes23 <andresccortes23@icloud.com>
2020-09-11 03:44:01 +0000

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    def squarenum = lambda nom: nom ** 2
-    newmatrix = [[squarenum(b) for b in a] for a in matrix]
+    newmatrix = [[b ** 2 for b in a] for a in matrix]
     return newmatrix
