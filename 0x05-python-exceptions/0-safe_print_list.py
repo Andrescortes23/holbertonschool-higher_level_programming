6a9f3cf965c34f668dbfed1354fb892ad4ec12cd
Andrescortes23 <andresccortes23@icloud.com>
2020-09-21 22:25:52 +0000

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    limit = 0;
+    for a in my_list:
+        try:
+            if limit < x:
+                print(a, end='')
+                limit += 1
+        except:
+            break
+    print()
+    return limit
