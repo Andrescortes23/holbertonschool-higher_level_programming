78e799bf32eda4637abb956adf955fa191c5dfb8
Andrescortes23 <andresccortes23@icloud.com>
2020-09-26 21:44:21 +0000

    Corrections os syntaxis and others

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,26 +11,28 @@ def matrix_divided(matrix, div):
     """
     tmp = []
     newlist = []
+    num = 0
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div is 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list) and len(matrix) is 0:
-        raise TypeError("matrix must be a matrix (list of lists) of\
+        raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
 
     for a in matrix:
+        if len(a) is not len(matrix[1]):
+            raise TypeError("Each row of the matrix must have the same size")
+
         if not isinstance(a, list) or len(a) is 0:
-            raise TypeError("matrix must be a matrix (list of lists) of\
+            raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
         tmp = []
         for b in a:
             if type(b) is not int and type(b) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of\
+                raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
             tmp.append(round(b / div, 2))
-        if len(a) is not len(tmp):
-            raise TypeError("Each row of the matrix must have the same size")
         newlist.append(tmp)
     return newlist
