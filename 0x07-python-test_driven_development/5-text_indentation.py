b4057db6f14ab0beed7be75863946d3db84d778d
Andrescortes23 <andresccortes23@icloud.com>
2020-09-26 01:55:45 +0000

    Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module with functions that print a text divide in parts"""
+
+def text_indentation(text):
+    """
+    Method to find certain characters in the passed text, when is founded
+    print two new lines and continue printing the text
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    cnt = 0
+    for char in text:
+        if text[cnt - 1] in (".", "?", ":") and char is " ":
+            cnt += 1
+            continue
+        print(char, end='')
+        if char in (".", "?", ":"):
+            print('\n')
+        cnt += 1
