b0b8b7073146c684d8036c93a6650af446e1f0b5
Andrescortes23 <andresccortes23@icloud.com>
2020-10-07 01:43:51 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to add method to class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """area is not implemented"""
+        raise Exception("area() is not implemented")
