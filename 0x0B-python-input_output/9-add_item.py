ef6926a32bac14657770904607789daca1c406f0
Andrescortes23 <andresccortes23@icloud.com>
2020-10-08 02:59:32 +0000

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,10 +6,10 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-myLista = []
+myList = []
 try:
     myList = load_from_json_file("add_item.json")
 except:
-    myLista = []
+    myList = []
 myList += sys.argv[1:]
 save_to_json_file(myList, "add_item.json")
