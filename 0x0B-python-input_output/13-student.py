fc56e306a4a19cae447ec9bc1e561b02f53e12b1
Andrescortes23 <andresccortes23@icloud.com>
2020-10-08 03:48:29 +0000

    Task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module class Student"""
+
+
+class Student:
+    """Class Studen"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function to get dict"""
+        if type(attrs) is list:
+            if all(isinstance(a, str) for a in attrs):
+                nwe = {}
+                for a in attrs:
+                    if a in self.__dict__:
+                        nwe[a] = self.__dict__[a]
+            return nwe
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Function  to replaces al attributes"""
+        for att in json:
+            self.__dict__[att] = json[att]
