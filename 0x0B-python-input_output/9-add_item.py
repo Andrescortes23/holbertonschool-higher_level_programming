793af731f4b781d958c009195cf1266876dc45bd
Andrescortes23 <andresccortes23@icloud.com>
2020-10-08 02:24:02 +0000

    Task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to add all arguments to python list and saved in file as json"""
+import sys
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+count = 1
+lista = []
+try:
+    lista = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    lista = []
+
+while count <= len(sys.argv) - 1:
+    lista.append(sys.argv[count])
+    count += 1
+
+save_to_json_file(lista, "add_item.json")
