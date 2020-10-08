8f537a11b8d9cfd017894a03db2c8435554beb39
Andrescortes23 <andresccortes23@icloud.com>
2020-10-08 02:34:55 +0000

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,15 +9,11 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-count = 1
 lista = []
 try:
     lista = load_from_json_file("add_item.json")
 except FileNotFoundError:
     lista = []
 
-while count <= len(argv) - 1:
-    lista.append(argv[count])
-    count += 1
-
+lista += argv[1:]
 save_to_json_file(lista, "add_item.json")
