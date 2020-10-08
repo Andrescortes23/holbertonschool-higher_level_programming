f3d999fe20a198f7145087ba47095803d78cb318
Andrescortes23 <andresccortes23@icloud.com>
2020-10-08 02:28:27 +0000

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Module to add all arguments to python list and saved in file as json"""
+
+
 import sys
 import json
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
