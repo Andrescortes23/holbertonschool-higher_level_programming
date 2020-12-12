cb7505c66c7b7814b378f4c459418c01d024ded2
Andrescortes23 <andresccortes23@icloud.com>
2020-12-12 22:19:36 +0000

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states\
-    WHERE name REGEXP '^[N]' ORDER BY id ASC")
+    WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
