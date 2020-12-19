44ffa3a746156a7c66744361f70963c8153dc0f2
Andrescortes23 <andresccortes23@icloud.com>
2020-12-19 01:56:23 +0000

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to passed URL, and displays the body of the response
-curl -Xs POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
