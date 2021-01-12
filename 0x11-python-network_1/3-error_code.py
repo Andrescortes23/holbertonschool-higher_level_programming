8e1f2a609f036f6e24e0d391e102e242e2962009
Andrescortes23 <andresccortes23@icloud.com>
2021-01-12 01:27:11 +0000

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ Send a request to the URL and displays the body of the response
 from urllib import request, error
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
