7237d3694672a375bacf28c7cd2ace5edbfa2679
Andrescortes23 <andresccortes23@icloud.com>
2021-01-12 02:07:53 +0000

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {"email". sys.argv[2]}
+    reqst = requests.post(url, data)
+    print(reqst.text)
