f7e924f8d989bdf275cf35ff8a213fd7518922da
Andrescortes23 <andresccortes23@icloud.com>
2021-01-13 02:32:02 +0000

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,6 +16,6 @@ else:
     if q.isalpha():
         resp = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
         resp = resp.json()
-        print("{} {}".format(resp.get('id'), resp.get('name')))
+        print("[{}] {}".format(resp.get('id'), resp.get('name')))
     else:
         print("No result")
