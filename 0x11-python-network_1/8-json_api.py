c2f4b8eadb602f5baa22a93580059c07d6530a2e
Andrescortes23 <andresccortes23@icloud.com>
2021-01-13 02:30:39 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter.
+"""
+
+
+import requests
+import sys
+
+if len(sys.argv) < 2:
+    q = ""
+    print("No result")
+else:
+    q = sys.argv[1]
+    if q.isalpha():
+        resp = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        resp = resp.json()
+        print("{} {}".format(resp.get('id'), resp.get('name')))
+    else:
+        print("No result")
