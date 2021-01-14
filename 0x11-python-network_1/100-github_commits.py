a6f033f31f48ebbde2fbf160af2cdc744ed57213
Andrescortes23 <andresccortes23@icloud.com>
2021-01-14 20:05:46 +0000

    Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,17 +11,8 @@ user = argv[2]
 resp = requests.get('https://api.github.com/repos/{}/{}/commits'
                     .format(user, repo))
 resp = resp.json()
-for a in resp:
-    for b in a:
-        if b == 'sha':
-            sha = a[b]
-        if b == 'commit':
-            for c in a[b]:
-                if c == 'author':
-                    for d in a[b][c]:
-                        if d == 'name':
-                            authname = a[b][c][d]
-                            iterat = 0
+iterat = 0
 while iterat < 10:
-    print("{}: {}".format(sha, authname))
+    print("{}: {}".format(resp[iterat].get('sha'),
+                          resp[iterat].get('commit').get('author').get('name')))
     iterat += 1
