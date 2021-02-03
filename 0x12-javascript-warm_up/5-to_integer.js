304a4deb5e218ef3a256b1aba24a065fe0a3f2b5
Andrescortes23 <andresccortes23@icloud.com>
2021-02-03 02:35:59 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+
 const val = parseInt(process.arg[2]);
 if (isNaN(val) === true) {
-    console.log("Not a number");
+  console.log('Not a number');
 } else {
-    console.log("My number:", val);
+  console.log('My number:', val);
 }
-
