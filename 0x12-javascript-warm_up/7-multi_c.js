2d963de0c38f8a87124704a34a0bac65e5a24c79
Andrescortes23 <andresccortes23@icloud.com>
2021-02-03 02:46:05 +0000

    Task 72

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
 const repeat = parseInt(process.argv[2]);
 let ind;
 if (isNaN(repeat) === true) {
-    console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
-    for (ind = 0; ind < repeat; ind++) {
-	console.log("C is fun");
-	}
-    }
+  for (ind = 0; ind < repeat; ind++) {
+    console.log('C is fun');
+  }
+}
